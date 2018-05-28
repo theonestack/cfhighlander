c32bb5ff79c60dd938df2f130818d325fed84016
toshke <n.tosic@base2services.com>
2018-05-28 18:24:08 +1000

    different int tests for PRs - no template validation and mocked azs

--- spec/test_component_spec.rb
+++ spec/test_component_spec.rb
@@ -15,11 +15,29 @@ RSpec.describe HighlanderCli, "#run" do
       end
       Git.clone 'https://github.com/theonestack/hl-component-test', wd, clone_opts
 
+      is_travis_pr = (ENV['TRAVIS'] and ENV['TRAVIS_PULL_REQUEST'].to_i > 0)
+
       Dir.chdir wd
       ARGV.clear
       ARGV << 'cfcompile'
       ARGV << 'test'
-      ARGV << '--validate'
+      ARGV << '--validate' unless is_travis_pr
+
+
+      default_maps = {
+          '123456789012' => {
+              'us-east-1' => {
+                  'Az0' => 'us-east-1a',
+                  'Az1' => 'us-east-1b',
+                  'Ac2' => 'us-east-1c'
+              }
+          }
+      }
+      # if running within travis and caused by PR, there are no creds available, thus
+      # we have to mock the azs
+      if is_travis_pr
+        File.write 'az.mappings.yaml', default_maps.to_yaml
+      end
 
       result = HighlanderCli.start
       expect(result).not_to eq(nil)
