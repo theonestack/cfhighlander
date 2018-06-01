25517181d568e361404c9ef30932a210baaaab74
Aaron Walker <aaronpwalker@gmail.com>
2018-06-01 13:21:55 +1000

    adds support for nested ip blocks and additional cidr block validation

--- cfndsl_ext/sg.rb
+++ cfndsl_ext/sg.rb
@@ -1,3 +1,4 @@
+require 'netaddr'
 
 def sg_create_rules (x, ip_blocks={})
   rules = []
@@ -15,12 +16,38 @@ end
 
 
 def lookup_ips_for_sg (ips, ip_block_name={})
+  cidr = []
   if ip_block_name == 'stack'
     cidr = [FnJoin( "", [ "10.", Ref('StackOctet'), ".", "0.0/16" ] )]
   elsif ips.has_key? ip_block_name
-    cidr = ips[ip_block_name]
+    ips[ip_block_name].each do |ip|
+      if (ips.include?(ip) || ip_block_name == 'stack')
+        cidr += lookup_ips_for_sg(ips, ip) unless ip == ip_block_name
+      else
+        if ip == 'stack'
+          cidr << [FnJoin( "", [ "10.", Ref('StackOctet'), ".", "0.0/16" ] )]
+        elsif(isCidr(ip))
+          cidr << ip
+        else
+          STDERR.puts("WARN: ip #{ip} is not a valid CIDR. Ignoring IP")
+        end
+      end
+    end
   else
-    cidr = [ip_block_name]
+    if isCidr(ip_block_name)
+      cidr = [ip_block_name]
+    else
+      STDERR.puts("WARN: ip #{ip_block_name} is not a valid CIDR. Ignoring IP")
+    end
   end
   cidr
+end
+
+def isCidr(block)
+  begin
+    NetAddr::CIDR.create(block)
+    return block.include?('/')
+  rescue NetAddr::ValidationError
+    return false
+  end
 end
