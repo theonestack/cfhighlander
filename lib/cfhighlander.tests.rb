d26aeaf74480399537f14ac3b5df4597441b9e3c
Guslington <guslington@gmail.com>
2019-01-23 18:57:19 +1100

    cftest command to run against test config on components

--- lib/cfhighlander.tests.rb
+++ lib/cfhighlander.tests.rb
@@ -1,9 +1,13 @@
 require 'yaml'
+<<<<<<< HEAD
 require 'json'
+=======
+>>>>>>> cftest command to run against test config on components
 
 module CfHighlander
   class Tests
 
+<<<<<<< HEAD
     attr_accessor :cases,
       :report,
       :exit_code,
@@ -56,11 +60,47 @@ module CfHighlander
           puts "\s\sTest: #{failure[:test]}"
           puts "\s\sType: #{failure[:type]}"
           puts "\s\sMessage: #{failure[:failure][:message]}"
+=======
+    attr_reader :test_cases, :failures, :exit_code
+
+    def initialize(component_name)
+      @component_name = component_name
+      @tests_file = "#{component_name}.tests.yaml"
+      @test_cases = load_test_cases
+      @failures = []
+      @exit_code = 0
+    end
+
+    def load_test_cases
+      tests = YAML.load(File.read(@tests_file))
+      @test_cases = tests.map { |test,config| { name: test, config: config } }
+    end
+
+    def failures(name,type,message)
+      @failures << { name: name, type: type, message: message }
+    end
+
+    def print_results
+      puts "\n\s\s============================"
+      puts "\s\s#    CfHighlander Tests    #"
+      puts "\s\s============================\n\n"
+      puts "\s\sPass: #{@test_cases.length - @failures.length}"
+      puts "\s\sFail: #{@failures.length}\n\n"
+
+      if @failures.any?
+        @exit_code = 1
+        puts "\s\s======= FAILURES ========"
+        @failures.each do |failure|
+          puts "\s\sTest: #{failure[:name]}"
+          puts "\s\sType: #{failure[:type]}"
+          puts "\s\sMessage: #{failure[:message]}"
+>>>>>>> cftest command to run against test config on components
           puts "\s\s-------------------------"
         end
       end
     end
 
+<<<<<<< HEAD
     def report_dir
       FileUtils.mkdir_p(@report_path) unless Dir.exist?(@report_path)
     end
@@ -109,5 +149,7 @@ module CfHighlander
       end
     end
 
+=======
+>>>>>>> cftest command to run against test config on components
   end
 end
