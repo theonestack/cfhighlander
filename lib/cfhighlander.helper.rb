db765920f21dcc2b3587336238231e204468a83f
Nikola Tosic <tosicnikola10@gmail.com>
2020-04-09 20:00:21 +1000

    allow params

--- lib/cfhighlander.helper.rb
+++ lib/cfhighlander.helper.rb
@@ -4,14 +4,19 @@ module Cfhighlander
   module Helper
 
 
-    def self.parameter_cfndsl_value(value)
+    def self.parameter_cfndsl_value(value, nested = false)
 
       if value.class == String
         return "'#{value}'"
       end
 
       if value.class == Hash
+        return value if nested
         return value.to_json
+      end
+
+      if value.class == Array
+        return value.collect { |it| self.parameter_cfndsl_value(it, nested = true) }
       end
 
       return "'#{value}'"
